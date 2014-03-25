66bb964d7e6db305799d2f26f616163cbe82ba6d
Graeme Wieden <graeme.wieden@gmail.com>
2014-03-25 20:08:43 +1000

    Changed default pulse length for protocol 1
    Can't really remember whether this was important or just for testing.
    Probably relates to one of the Swann door chimes.

--- RCSwitch/RCSwitch.cpp
+++ RCSwitch/RCSwitch.cpp
@@ -57,7 +57,7 @@ void RCSwitch::setProtocol(int nProtocol)
 	this->nProtocol = nProtocol;
 	if (nProtocol == 1)
 	{
-		this->setPulseLength(350);
+		this->setPulseLength(450);
 	}
 	else if (nProtocol == 2) {
 		this->setPulseLength(650);
